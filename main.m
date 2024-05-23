0cd27692af1f6eae7877d8954a845c1b53acd6bb
rezaKeshtkarzadeh <129211850+rezaKeshtkarzadeh@users.noreply.github.com>
2024-05-23 17:27:15 +0330

    Update main.m

--- main.m
+++ main.m
@@ -3,8 +3,8 @@
 clear; clc; format long;
 
 %% init and callbacks -----------------------------------------------------
-w1 = 5000;                        % Width of main square
-n = 2000;                         % Number of squares to be drawn
+w1 = 10;                        % Width of main square
+n = 50;                         % Number of squares to be drawn
 i = 0.1;                        % 0 <= i <= 1
 
 nested_squares(w1, n, i);       % Callback function
