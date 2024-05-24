05fc789fb15be560c3b43ff106cde339dc10bf5b
rezaKeshtkarzadeh <129211850+rezaKeshtkarzadeh@users.noreply.github.com>
2024-05-24 08:09:43 +0330

    deleted not relative figs.

--- main.m
+++ main.m
@@ -4,7 +4,7 @@ clear; clc; format long;
 
 %% init and callbacks -----------------------------------------------------
 w1 = 10;                        % Width of main square
-n = 100;                         % Number of squares to be drawn
+n = 50;                         % Number of squares to be drawn
 i = 0.1;                        % 0 <= i <= 1
 
 nested_squares(w1, n, i);       % Callback function
